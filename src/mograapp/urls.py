cb02415015c3604c81a53878ca2a6f90c4790449
hitugihane <hituzihane01@gmail.com>
2023-03-07 01:32:22 +0900

    詳細表示画面とmodelsの修正

--- src/mograapp/urls.py
+++ src/mograapp/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', views.MyLoginView.as_view(), name='login'),
     path('logout/', views.MyLogoutView.as_view(), name='logout'),
     path('home/', views.MyUserView.as_view(), name='home'),
+    path('event/<uuid:uuid>/', views.EventDetailView.as_view(), name='event_detail'),
 ]
