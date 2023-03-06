c8a4f085c0521e520be6d3f7de1b777afb688b9a
AndyMJohnson <masa.kuri0711@gmail.com>
2023-03-06 19:06:52 +0900

    change urls.py and add graph.html

--- src/mograapp/urls.py
+++ src/mograapp/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('mogra/', views.mografunction),
     path('signup/', views.MySignupView.as_view(), name='signup'),
     path('login/', views.MyLoginView.as_view(), name='login'),
+    path('graph/', views.MyLoginView.as_view(), name='graph'),
     #path('logout/', views.MyLogoutView.as_view(), name='logout'),
     #path('user/', views.MyUserView.as_view(), name='user'),
     #path('other/', views.MyOtherView.as_view(), name='other'),
