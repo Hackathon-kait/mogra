c51413f659dcc7ec0d093d6b3b5d90047694d0d5
Naoki-02 <smile.naonao@icluod.com>
2023-03-07 20:09:21 +0900

    イベント作成機能実装完了

--- src/mograapp/forms.py
+++ src/mograapp/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from .models import EventsModel
+from django.forms.widgets import DateInput
+
+
 
 class SignupForm(UserCreationForm):
     class Meta:
@@ -10,4 +13,26 @@ class SignupForm(UserCreationForm):
         
 class LoginForm(AuthenticationForm):
     pass
+
+class EventsModelForm(forms.ModelForm):
+    class Meta:
+        model = EventsModel
+        fields = ['title', 'detail', 'evaluation','date_at']
+
+        
+        
+        labels = {
+            'title': 'タイトル',
+            'detail': '詳細',
+            'eevaluation': '評価',
+            'date_at': '日時',
+        }
+        widgets = {
+                'date_at': DateInput(attrs={'type': 'date'})
+        }
+    def clean_evaluation(self):
+        value = self.cleaned_data['evaluation']
+        if value >= 11:
+            raise forms.ValidationError('10以下の値を入力してください。')
+        return value
 
