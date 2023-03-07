d08900e8f572a448b1e16a71c6b8bbcb4d25619a
siarena1314 <ttm2dxhk7rtxkeqeejce@gmail.com>
2023-03-07 21:34:06 +0900

    サインアップページ

--- src/app/settings.py
+++ src/app/settings.py
@@ -134,7 +134,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    BASE_DIR / '/static/',
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
