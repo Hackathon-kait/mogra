c00fd71e355fb6aa290e1ff4b23060cf554f50cc
Naoki-02 <smile.naonao@icluod.com>
2023-03-07 22:50:47 +0900

    タイトルを変更した

--- src/mograapp/urls.py
+++ src/mograapp/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.MySignupView.as_view(), name='signup'),
-    path('mogra/', views.mografunction),
+    path('',views.MograView.as_view(), name='mogra'),
     path('signup/', views.MySignupView.as_view(), name='signup'),
     path('login/', views.MyLoginView.as_view(), name='login'),
     path('logout/', views.MyLogoutView.as_view(), name='logout'),
     path('home/', views.MyUserView.as_view(), name='home'),
     path('event/<uuid:uuid>/', views.EventDetailView.as_view(), name='event_detail'),
+    path('create/',views.MyEventCreateView.as_view(),name='create'),
 ]
